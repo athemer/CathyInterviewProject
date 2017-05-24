498bcfbafc7ada859f371b557df25b04de35a6ce
athemer <athemer0520@gmail.com>
2017-05-24 23:13:40 +0800

    xid done

---
+++ CathyInterviewProject/ParkTableViewCell.h
@@ -0,0 +1,23 @@
+//
+//  ParkTableViewCell.h
+//  CathyInterviewProject
+//
+//  Created by 陳冠華 on 2017/5/24.
+//  Copyright © 2017年 my app. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ParkTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *idLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *parkName;
+
+@property (weak, nonatomic) IBOutlet UILabel *name;
+
+@property (weak, nonatomic) IBOutlet UIImageView *image;
+
+@property (weak, nonatomic) IBOutlet UILabel *introduction;
+
+@end
