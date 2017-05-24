153f2d07e69630769e22912a37a7986d1e18eb77
athemer <athemer0520@gmail.com>
2017-05-24 16:44:49 +0800

    first

---
+++ CathyInterviewProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CathyInterviewProject
+//
+//  Created by 陳冠華 on 2017/5/24.
+//  Copyright © 2017年 my app. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CathyInterviewProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CathyInterviewProject
+//
+//  Created by 陳冠華 on 2017/5/24.
+//  Copyright © 2017年 my app. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
